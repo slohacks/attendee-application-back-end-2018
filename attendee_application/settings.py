0cb90d520b6f62e32362893af3d1efe97aa082cc
stonewallstan <stanleyarmstrong31@gmail.com>
2018-08-18 15:48:57 -0700

    refactor(./attendee_application/settings.py): add environment variables for the postgres database

--- attendee_application/settings.py
+++ attendee_application/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'attendee_application.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'attendee_backend',
-        'USER': 'stonewallstan',
-        'PASSWORD': 'SLo_h4ck5',
-        'HOST': 'localhost',
+        'NAME': os.environ.get('DB_NAME', ''),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASS', ''),
+        'HOST': os.environ.get('DB_HOST', ''),
         'ROOT': '',
     }
 }
