90260fb8ef89dae491d51eebefa8b90e76cf0856
stonewallstan <stanleyarmstrong31@gmail.com>
2018-08-18 15:18:37 -0700

    feat(all): migrate to PostgresSQL

--- attendee_application/settings.py
+++ attendee_application/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'attendee_application.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'attendee_backend',
+        'USER': 'stonewallstan',
+        'PASSWORD': 'SLo_h4ck5',
+        'HOST': 'localhost',
+        'ROOT': '',
     }
 }
 
