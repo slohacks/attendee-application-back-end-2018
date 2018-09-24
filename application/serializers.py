b20d86761ee8df1ba36131a5a9245ea606ecac73
Justin Mo <jmo02@calpoly.edu>
2018-09-24 13:17:32 -0700

    feat(views.py): add function based views (#17)
    * put-delete
    
    * feat(application): refactor application views and include put and delete views
    
    * fix(application): add resume to gitignore
    
    * fix(application): re-add resume to gitignore
    
    * fix(application/views.py): fix 404 for PUT in views.py

--- application/serializers.py
+++ application/serializers.py
@@ -8,7 +8,7 @@ from rest_framework import permissions
 class ApplicationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
        model = Application
-       fields = ('url', 'id','first_name', 'last_name', 'email', 
+       fields = ('id','first_name', 'last_name', 'email', 
        'phone_number','is_eighteen', 'school', 'graduation_date', 'major',
         'city', 'dietary_restrictions', 'allergies', 'github', 'linkedin',
         'personal_website', 'resume', 'short_answer', 'gender', 'ethnicity',
--- application/urls.py
+++ application/urls.py
@@ -1,17 +1,20 @@
-from django.conf.urls import url, include
+from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from application import views
 from rest_framework import renderers
 from rest_framework.routers import DefaultRouter
 from rest_framework.schemas import get_schema_view
 
-router = DefaultRouter()
-router.register(r'application', views.ApplicationViewSet)
-router.register(r'user', views.UserViewSet)
-schema_view = get_schema_view(title = 'Pastebin API')
+# router = DefaultRouter()
+# # router.register(r'application', views.ApplicationViewSet)
+# router.register(r'user', views.UserViewSet)
+# schema_view = get_schema_view(title = 'Pastebin API')
 
 urlpatterns = [
-   url(r'^', include(router.urls)),
-   url(r'^schema/$', schema_view),
-   url(r'^questions/$', views.questions),
+#    url(r'^', include(router.urls)),
+#    url(r'^schema/$', schema_view),
+    path('applications/', views.application_list, name="application_list"),
+    path('applications/<int:pk>', views.application_detail, name="application_detail"),
+    path('applications/<int:pk>/<value>', views.application_detail, name="application_detail"),
+    path('questions/', views.questions, name="questions"),
 ]
