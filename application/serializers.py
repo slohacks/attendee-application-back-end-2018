2073a95dabe83c324845dfafb44bac23dfe3420f
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2018-10-08 12:24:10 -0700

    feat(./application): implement input validation and add `other` fields (#14)
    * feat(all): migrate to PostgresSQL
    
    * refactor(./attendee_application/settings.py): add environment variables for the postgres database
    
    * docs(requirements.txt): add requirements.txt
    
    * feat(./application/models.py): change CharFields
    
    * feat(./application): add input validation on github and linkedin fields
    
    * feat(./application/serializers.py): add validation to date and boolean fields
    
    #13
    
    * chore(.gitignore): ignore resume files
    
    * chore(./resume): delete files for .gitignore to work
    
    * test(application/tests.py): adding tests, currently they aren't working
    
    * test(application/tests.py): still debugging tests, waiting on function based views
    
    * feat(./application/models.py and serializers.py): add other fields
    
    * test(./application/tests.py): revert tests temporarily to add features

--- application/serializers.py
+++ application/serializers.py
@@ -6,13 +6,62 @@ from rest_framework import permissions
 
 
 class ApplicationSerializer(serializers.HyperlinkedModelSerializer):
+    # Fields that need validation
+    github = serializers.URLField(max_length = 250)
+    linkedin = serializers.URLField(max_length = 250)
+    graduation_date = serializers.DateField()
+    is_eighteen = serializers.BooleanField(default = False)
+    agreed = serializers.BooleanField(default = False)
+    other_dietary_restrictions = serializers.CharField(allow_blank = True)
+    allergies = serializers.CharField(allow_blank = True)
+    other_gender_field = serializers.CharField(allow_blank = True)
+    other_ethnicity = serializers.CharField(allow_blank = True)
+    anything_else = serializers.CharField(allow_blank = True)
+
+    # Validators for fields
+    def validate_github(self, value):
+        if 'https://github.com/' not in value:
+            raise serializers.ValidationError('Invalid Github link,' +
+                ' please make sure that the link contains http://github.com/')
+        return value
+    def validate_linkedin(self, value):
+        if 'https://www.linkedin.com/in/' not in value:
+            raise serializers.ValidationError('Invalid LinkedIn link, ' +
+                'please make sure that the link contains http://linkedin.com/in/')
+        return value
+    def validate_graduation_date(self, value):
+        year = 2019
+        month = 2
+        day = 3
+        if value.year < year:
+            raise serializers.ValidationError(
+                'Invalid Date: Applicant graduated before the event')
+        elif value.year == year:
+            if value.month < month:
+                raise serializers.ValidationError(
+                    'Invalid Date: Applicant graduated before the event')
+            if value.month == month:
+                if value.day <= day:
+                    raise serializers.ValidationError(
+                        'Invalid Date: Applicant graduated before the event')
+        return value
+    def validate_is_eighteen(self, value):
+        if not value:
+            raise serializers.ValidationError('Invalid Age: Applicant must be 18')
+        return value
+    def validate_agreed(self, value):
+        if not value:
+            raise serializers.ValidationError('Invalid: Applicant must agree' + 
+            ' to the code of conduct')
+        return value
+
     class Meta:
        model = Application
        fields = ('id','first_name', 'last_name', 'email', 
        'phone_number','is_eighteen', 'school', 'graduation_date', 'major',
-        'city', 'dietary_restrictions', 'allergies', 'github', 'linkedin',
-        'personal_website', 'resume', 'short_answer', 'gender', 'ethnicity',
-        'anything_else', 'agreed')
+        'city', 'dietary_restrictions','other_dietary_restrictions', 'allergies',
+         'github', 'linkedin','personal_website', 'resume', 'short_answer',
+          'gender', 'other_gender_field', 'ethnicity', 'other_ethnicity', 'anything_else', 'agreed')
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
